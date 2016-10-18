1f98afcfc2bda06bf0e29143d3661bae0d564918
DESKTOP-UP4L2GA\poluo <fengnanchang@gmail.com>
2016-10-18 20:12:35 +0800

    增加了控制台清除一行的宏定义

--- Command_Line/platform.h
+++ Command_Line/platform.h
@@ -15,14 +15,17 @@
 	#define PRINT_STR printf("%s\n",str_tmp)
 	#define BACK_LINE_HEAD printf("\r\033[k")
 	#define ENTER_KEY '\n'
-	#define MOVEDOWN(x) printf("\033[%dB", (x))
+	
 #elif _WIN32
 	#define GET_CH _getche()
 	#define PRINT_STR printf("\n%s\n",str_tmp)
 	#define BACK_LINE_HEAD printf("\r")
 	#define ENTER_KEY '\r'
-	#define MOVEDOWN(x) printf("\033[%dB", (x)) ///?? in WIN32
+	
 #endif
+
+#define CLEAR_LINE printf("\33[2K\r")
+#define MOVEDOWN(x) printf("\033[%dB", (x))
 
 
 #ifdef linux

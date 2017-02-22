bd6eb892b71a130dbdd511924f5386590aa53154
poluo <fengnanchang@gmail.com>
2017-02-22 21:50:00 +0800

    优化51ape.com，爬取hiflac.com

--- pythonTools/scrapy/flac/flac/spiders/a51ape.py
+++ pythonTools/scrapy/flac/flac/spiders/a51ape.py
@@ -14,6 +14,10 @@ class A51apeSpider(CrawlSpider):
     rules = [
         Rule(LinkExtractor(allow=("http://www.51ape.com/.+yinyue/")), callback='parse_yinyue', follow=True),
         Rule(LinkExtractor(allow=("/.+yinyue/index")), callback='parse_yinyue', follow=True),
+        Rule(LinkExtractor(allow=("/.+yueyu")), callback='parse_yinyue', follow=True),
+        Rule(LinkExtractor(allow=("/.+yingwen")), callback='parse_yinyue', follow=True),
+        Rule(LinkExtractor(allow=("/.+经典歌曲")), callback='parse_yinyue', follow=True),
+        Rule(LinkExtractor(allow=("/.+zhuanji")), callback='parse_zhuanji', follow=True),
     ]
 
     def parse_yinyue(self, response):
@@ -36,6 +40,28 @@ class A51apeSpider(CrawlSpider):
                                      callback=self.prase_song)
             request.meta['item'] = item
             yield request
+
+    def parse_zhuanji(self, response):
+        element_list = response.css('body > div.bg_wh.all.m > div > div.fl.over.w638 > div.fl.over.w638.mt_2 > '
+                                    'div.news.over.fl > ul > li')
+        item_list = []
+        for one in element_list:
+            item = flacItem()
+            tmp = one.css('a::attr(title)').extract_first()
+            try:
+                item['singer_name'] = tmp.split('-')[0]
+                tmp = tmp.split('-')[1].split('.')
+                item['format'] = tmp[1]
+            except IndexError:
+                continue
+            item['href'] = one.css('a::attr(href)').extract_first()
+            item['size'] = one.css('span.fr.c999::text').extract_first()
+            item_list.append(item)
+            request = scrapy.Request(response.urljoin(item['href']),
+                                     callback=self.prase_song)
+            request.meta['item'] = item
+            yield request
+
 
     def prase_song(self, response):
         element = response.css('body > div.bg_wh.all.m > div > div.fl.over.w638 > h1::text').extract_first()
